3005710017490d54fdbd4902e13a29f4c942abc6
Valentin Vasilyev <iamvalentin@gmail.com>
2011-09-17 18:44:30 +0400

    Added problem 007 solution.

---
+++ euler-csharp/euler-csharp/Problem007.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace euler_csharp {
+    public class Problem007 {
+        public static int Solution() {
+            return Prime.Sequence().Skip(10001).Take(1).First();
+        }
+    }
+}
--- euler-csharp/euler-csharp/Program.cs
+++ euler-csharp/euler-csharp/Program.cs
@@ -1,11 +1,12 @@
 ﻿
 using System;
+using System.Linq;
 
 
 namespace euler_csharp {
     class Program {
         static void Main() {
-            Console.WriteLine(Problem006.Solution());
+            Console.WriteLine(Problem007.Solution());
         }
     }
 }
